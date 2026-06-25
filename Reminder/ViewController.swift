a727d7ecad2875fbc0b3692c43b1dd64ebc60aad
Jean Paul Rojas <ingjeanpaulrojas@gmail.com>
2026-06-25 14:16:20 -0300

    Initial Commit

---
+++ Reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by Jean Paul Rojas on 25/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
